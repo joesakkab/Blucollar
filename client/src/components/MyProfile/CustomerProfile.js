50d17f69b3902e1b2f91bb5943139541260c02fd
twarep <ptwareck@uwaterloo.ca>
2023-03-25 13:35:53 -0400

    edit button conditionally renders based on editing state

--- client/src/components/MyProfile/CustomerProfile.js
+++ client/src/components/MyProfile/CustomerProfile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Grid, Card, CardContent, Typography, Button, } from '@material-ui/core';
 import useStyles from '../Profile/Styles';
 import * as FIELDS from '../../constants/customerConst';
@@ -7,56 +7,77 @@ export default function CustomerProviderProfile(props) {
 
     const obj = props.profileData;
     const id = obj['cust_id']
-    console.log(id)
+
+    const [fName, setFName] = useState(obj[FIELDS.FIRST]);
+    const [LName, setLName] = useState(obj[FIELDS.LAST]);
+    const [email, setEmail] = useState(obj[FIELDS.EMAIL]);
+    const [location, setLocation] = useState(obj[FIELDS.LOCATION]);
+    const [readOnlyState, setReadOnlyState] = useState(true);
 
     const handleEdit = () => {
-        
+      setReadOnlyState(false);
     }
     
     const classes = useStyles();
     return (
         <form>
-                <div className={classes.listing}>
-                <Grid container spacing={1}>
-                    <Grid item xs={12}><Typography variant="h6">Your Profile Details:</Typography></Grid>
-                    <Grid item xs={12}>
-                    <Card>
-                        <CardContent>
-                            <TextField 
-                            label="First Name" 
-                            className={classes.test2} 
-                            value = {obj[FIELDS.FIRST]}
-                            />
-                            <TextField 
-                            label="Last Name" 
-                            className={classes.test2}
-                            value = {obj[FIELDS.LAST]}
-                            />
-                            <TextField 
-                            label="Email" 
-                            className={classes.test2}
-                            value = {obj[FIELDS.EMAIL]}
-                            />
-                            <TextField 
-                            label="Location" 
-                            className={classes.test2} 
-                            value = {obj[FIELDS.LOCATION]}
-                            />
-                        </CardContent>
-                    </Card>
-                    </Grid>
-                </Grid>
-                </div>
+          <div className={classes.listing}>
+          <Grid container spacing={1}>
+              <Grid item xs={12}><Typography variant="h6">Your Profile Details:</Typography></Grid>
+              <Grid item xs={12}>
+              <Card>
+                  <CardContent>
+                      <TextField
+                      label="First Name" 
+                      className={classes.test2} 
+                      value={fName}
+                      onChange={(fName) => setFName(fName.target.value)}
+                      inputProps={{ 
+                        maxLength: 30,
+                        readOnly: readOnlyState }}
+                      />
+                      <TextField 
+                      label="Last Name" 
+                      className={classes.test2}
+                      value={LName}
+                      onChange={(LName) => setLName(LName.target.value)}
+                      inputProps={{ 
+                        maxLength: 30,
+                        readOnly: readOnlyState }}
+                      />
+                      <TextField 
+                      label="Email"
+                      className={classes.test2}
+                      value={email}
+                      onChange={(email) => setEmail(email.target.value)}
+                      inputProps={{ 
+                        maxLength: 30,
+                        readOnly: readOnlyState }}
+                      />
+                      <TextField 
+                      label="Location" 
+                      className={classes.test2} 
+                      value={location}
+                      onChange={(location) => setLocation(location.target.value)}
+                      inputProps={{ 
+                        maxLength: 30,
+                        readOnly: readOnlyState }}
+                      />
+                  </CardContent>
+              </Card>
+              </Grid>
+          </Grid>
+          </div>
             
-            <Grid sx={classes.paper}>
-              <Button
-              type='submit'
-              onClick={handleEdit}>
-              Edit Profile
-              </Button>
-            </Grid>
+          <Grid sx={classes.paper}>
+            <Button
+            disabled={!readOnlyState}
+            type='button'
+            onClick={handleEdit}>
+            {readOnlyState ? 'Edit Profile' : 'Editing Profile'}
+            </Button>
+          </Grid>
                        
-               
         </form>
         
     )
